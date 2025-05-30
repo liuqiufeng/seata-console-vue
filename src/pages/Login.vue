52d0e49dd2ce91487498544224ba40a93e496d72
liuqiufeng <775038282@qq.com>
2025-05-30 09:30:36 +0800

    Improve login page

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -20,28 +20,20 @@
       <v-img
         class="mx-auto my-6"
         max-width="228"
-        src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
+        src="@/assets/seata_logo.png"
       />
 
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+      <div class="text-subtitle-1 text-medium-emphasis">Username</div>
 
       <v-text-field
         density="compact"
-        placeholder="Email address"
+        placeholder="Username"
         prepend-inner-icon="mdi-email-outline"
         variant="outlined"
       />
 
-      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+      <div class="text-subtitle-1 text-medium-emphasis">
         Password
-
-        <a
-          class="text-caption text-decoration-none text-blue"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Forgot login password?</a>
       </div>
 
       <v-text-field
@@ -60,7 +52,7 @@
         variant="tonal"
       >
         <v-card-text class="text-medium-emphasis text-caption">
-          Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three hours. If you must login now, you can also click "Forgot login password?" below to reset the login password.
+          Warning: Internal system, cannot be exposed to the public network
         </v-card-text>
       </v-card>
 
@@ -73,17 +65,6 @@
       >
         Log In
       </v-btn>
-
-      <v-card-text class="text-center">
-        <a
-          class="text-blue text-decoration-none"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Sign up now <v-icon icon="mdi-chevron-right" />
-        </a>
-      </v-card-text>
     </v-card>
   </div>
 </template>
@@ -154,7 +135,7 @@ $animationDuration: 2s;
   .product-card{
     position: absolute;
     left: 0;
-    top: 30%;
+    top: 40%;
     margin-top: -47px;
     width: 600px;
     margin-left: 40px;
