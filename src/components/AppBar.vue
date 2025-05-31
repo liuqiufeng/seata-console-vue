de63e68d046d3704de8e9d736e74ad1731b52830
liuqiufeng <775038282@qq.com>
2025-06-01 01:32:55 +0800

    optimize official website url

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,17 +1,17 @@
 <template>
   <v-app-bar>
     <div class="app-bar-logo">
-      <a href="https://seata.apache.org/" rel="noopener noreferrer" target="_blank">
+      <a :href="baseWebsiteUrl" rel="noopener noreferrer" target="_blank">
         <v-img src="@/assets/seata_logo.png" />
       </a>
     </div>
     <template #append>
       <div class="app-bar-menu d-flex ga-4 align-center">
-        <a href="https://seata.apache.org/" rel="noopener noreferrer" target="_blank">{{ t('appBar.home') }}</a>
-        <a href="https://seata.apache.org/docs/overview/what-is-seata/" rel="noopener noreferrer" target="_blank">{{ t('appBar.docs') }}</a>
-        <a href="https://seata.apache.org/blog" rel="noopener noreferrer" target="_blank">{{ t('appBar.blog') }}</a>
-        <a href="https://seata.apache.org/community" rel="noopener noreferrer" target="_blank">{{ t('appBar.community') }}</a>
-        <a href="https://seata.apache.org/download/seata-server" rel="noopener noreferrer" target="_blank">{{ t('appBar.download') }}</a>
+        <a :href="baseWebsiteUrl" rel="noopener noreferrer" target="_blank">{{ t('appBar.home') }}</a>
+        <a :href="baseWebsiteUrl + 'docs/overview/what-is-seata/'" rel="noopener noreferrer" target="_blank">{{ t('appBar.docs') }}</a>
+        <a :href="baseWebsiteUrl + 'blog'" rel="noopener noreferrer" target="_blank">{{ t('appBar.blog') }}</a>
+        <a :href="baseWebsiteUrl + 'community'" rel="noopener noreferrer" target="_blank">{{ t('appBar.community') }}</a>
+        <a :href="baseWebsiteUrl + 'download/seata-server'" rel="noopener noreferrer" target="_blank">{{ t('appBar.download') }}</a>
         <v-btn border icon @click="changeLocale">{{ t('appBar.localeSwitchBtn') }}</v-btn>
         <v-btn v-if="isLogin" icon="mdi-account-circle" />
       </div>
@@ -23,6 +23,10 @@
 
   const { t,locale } = useI18n()
   const isLogin = ref(true);
+
+  const baseWebsiteUrl = computed(() => {
+    return locale.value === 'en'? 'https://seata.apache.org/':'https://seata.apache.org/zh-cn/';
+  });
 
   const changeLocale = () => {
     locale.value = locale.value === 'en' ? 'zhHans' : 'en';
