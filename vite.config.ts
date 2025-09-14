c58671dfa34ea9ef26a1c6a313b7e99232552136
liuqiufeng <775038282@qq.com>
2025-09-14 17:42:33 +0800

    debug deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
