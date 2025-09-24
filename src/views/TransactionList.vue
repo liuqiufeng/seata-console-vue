2856bd847422ca9d1e5c94c4ac60f84346e50fc9
liuqiufeng <775038282@qq.com>
2025-09-24 23:33:35 +0800

    page header

--- src/views/TransactionList.vue
+++ src/views/TransactionList.vue
@@ -6,12 +6,21 @@ const routes = [
 </script>
 
 <template>
-  <a-page-header
-    style="border: 1px solid rgb(235, 237, 240)"
-    title="Title"
-    :breadcrumb="{ routes }"
-    sub-title="This is a subtitle"
-  />
+  <a-page-header title="事务列表" :breadcrumb="{ routes }" sub-title="全局事务列表" />
+  <a-card>
+    <a-form layout="inline" style="margin: 16px 0">
+      <a-form-item label="测试">
+        <a-input placeholder="placeholder" />
+      </a-form-item>
+      <a-form-item label="测试">
+        <a-input placeholder="placeholder" />
+      </a-form-item>
+      <a-form-item label="测试">
+        <a-input placeholder="placeholder" />
+      </a-form-item>
+      <a-button type="primary" html-type="button">搜索</a-button>
+    </a-form>
+  </a-card>
 </template>
 
 <style scoped lang="less"></style>
