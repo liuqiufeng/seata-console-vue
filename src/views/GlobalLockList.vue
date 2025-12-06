5d9a70b87ef05e1bd49abfedb050634329557a3d
liuqiufeng <775038282@qq.com>
2025-12-06 12:11:20 +0800

    improve global lock page

--- src/views/GlobalLockList.vue
+++ src/views/GlobalLockList.vue
@@ -1,7 +1,76 @@
 <script setup lang="ts">
+import type { TableColumnsType } from 'ant-design-vue'
+
 const routes = [
   { breadcrumbName: 'Home', path: '' },
   { breadcrumbName: 'GlobalLock List', path: 'globallock/list' },
+]
+
+const columns: TableColumnsType = [
+  {
+    title: 'xid',
+    dataIndex: 'xid',
+    key: 'xid',
+  },
+  {
+    title: 'transactionId',
+    dataIndex: 'transactionId',
+    key: 'transactionId',
+  },
+  {
+    title: 'branchId',
+    dataIndex: 'branchId',
+    key: 'branchId',
+  },
+  {
+    title: 'resourceId',
+    dataIndex: 'resourceId',
+    key: 'resourceId',
+  },
+  {
+    title: 'tableName',
+    dataIndex: 'tableName',
+    key: 'tableName',
+  },
+  {
+    title: 'pk',
+    dataIndex: 'pk',
+    key: 'pk',
+  },
+  {
+    title: 'rowKey',
+    dataIndex: 'rowKey',
+    key: 'rowKey',
+  },
+  {
+    title: 'gmtCreate',
+    dataIndex: 'gmtCreate',
+    key: 'gmtCreate',
+  },
+  {
+    title: 'gmtModified',
+    dataIndex: 'gmtModified',
+    key: 'gmtModified',
+  },
+  {
+    title: '操作',
+    key: 'action',
+    fixed: 'right',
+    width: 300,
+  },
+]
+const data = [
+  {
+    xid: 'test-xid-001',
+    transactionId: '1234567890',
+    branchId: '9876543210',
+    resourceId: 'resource-001',
+    tableName: 'user_table',
+    pk: '1',
+    rowKey: 'row-001',
+    gmtCreate: '2024-01-01 10:00:00',
+    gmtModified: '2024-01-01 12:00:00',
+  },
 ]
 </script>
 
@@ -49,7 +118,15 @@ const routes = [
       </a-form-item>
     </a-form>
   </a-card>
-  <a-table />
+  <a-table :columns="columns" :data-source="data" :scroll="{ x: 1300 }">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'action'">
+        <span>
+          <a>删除全局锁</a>
+        </span>
+      </template>
+    </template>
+  </a-table>
 </template>
 
 <style scoped lang="less">
